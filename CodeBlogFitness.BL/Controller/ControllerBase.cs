5b9c80c5f98788ac3f74e588ce26cf9402e8dd7f
e-v-antonov <antonov.eugeny2012@yandex.ru>
2022-01-19 18:11:42 +0300

    Реализован пример мезанизма DI (внедрение зависимостей).

--- CodeBlogFitness.BL/Controller/ControllerBase.cs
+++ CodeBlogFitness.BL/Controller/ControllerBase.cs
@@ -9,18 +9,14 @@ namespace CodeBlogFitness.BL.Controller
     /// </summary>
     public abstract class ControllerBase
     {
+        protected IDataSaver saver = new SerializeDataSaver();
+
         /// <summary>
         /// Сохранить данные.
         /// </summary>
         protected void Save(string fileName, object item)
         {
-            var formatter = new BinaryFormatter();
-
-            using (var fs = new FileStream(fileName, FileMode.OpenOrCreate))
-            {
-                formatter.Serialize(fs, item);
-
-            }
+            saver.Save(fileName, item);
         }
 
         /// <summary>
@@ -28,19 +24,7 @@ namespace CodeBlogFitness.BL.Controller
         /// </summary>
         protected T Load<T>(string fileName)
         {
-            var formatter = new BinaryFormatter();
-
-            using (var fs = new FileStream(fileName, FileMode.OpenOrCreate))
-            {
-                if (fs.Length > 0 && formatter.Deserialize(fs) is T items)
-                {
-                    return items;
-                }
-                else
-                {
-                    return default(T);
-                }
-            }
+            return saver.Load<T>(fileName);
         }
     }
 }
